9e19fda058eb739c312e62aec6943c6418d2d0a2
guillaumepn <guillaumepn@free.fr>
2017-02-01 16:31:34 +0100

    Clean le fichier Test avec php-cs-fixer

--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -4,42 +4,47 @@ namespace tests\Lib;
 
 use Lib\StringFormatter;
 
-class StringFormatterTest extends \PHPUnit_Framework_TestCase {
-
-    public function testPrefix() {
+class StringFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPrefix()
+    {
         $myclass = new StringFormatter();
         $camelCase = true;
         $result = $myclass->prefix("pre", "test", $camelCase);
 
-        if ($camelCase)
+        if ($camelCase) {
             $this->assertSame($result, "preTest");
-        else
+        } else {
             $this->assertSame($result, "pretest");
+        }
     }
 
-    public function testSuffix() {
+    public function testSuffix()
+    {
         $myclass = new StringFormatter();
         $camelCase = false;
         $result = $myclass->suffix("suf", "test", $camelCase);
 
-        if ($camelCase)
+        if ($camelCase) {
             $this->assertSame($result, "testSuf");
-        else
+        } else {
             $this->assertSame($result, "testsuf");
+        }
     }
 
-    public function testToCamelCase() {
+    public function testToCamelCase()
+    {
         $myclass = new StringFormatter();
         $result = $myclass->toCamelCase("first", "second");
 
         $this->assertSame($result, "firstSecond");
     }
 
-    public function testConcatString() {
+    public function testConcatString()
+    {
         $myclass = new StringFormatter();
         $result = $myclass->concatString("first", "second");
 
         $this->assertSame($result, "firstsecond");
     }
-
 }
